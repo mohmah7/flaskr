7a746ef999fc79727dd88b5e71875fbd6581094b
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-29 21:22:52 +0200

    Update picture.py

--- picture.py
+++ picture.py
@@ -3,4 +3,18 @@ app = Flask(__name__)
 
 @app.route('/picture')
 def hello_world():
-    return 'Hello, World! From Egypt'
+    return """
+<html><head>
+<title>Hello, World! From Egypt' </title>
+</head>
+<body>
+<h1>  The numbers are  </h1>
+<hr>
+<FORM value="form" action="index" method="post">
+   <P>
+	 <INPUT type="submit" value="Return to index page">
+   </P>
+</FORM>
+</body>
+</html>
+"""
