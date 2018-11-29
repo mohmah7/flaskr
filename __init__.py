2aaac186dc5deb792c92f89ee592776f15719985
mohmah7 <mohmah7@users.noreply.github.com>
2018-11-29 19:02:37 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,17 +6,17 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    #app.config.from_mapping(
+     #   SECRET_KEY='dev',
+      #  DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    #)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
-    else:
+    #else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        #app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
