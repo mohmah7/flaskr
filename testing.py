eca64758ade64a2c66974720b74d884e02abaf48
mohmah7 <mohmah7@users.noreply.github.com>
2018-12-01 07:10:20 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -32,6 +32,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True) 
     return app
 
+app = create_app()
+
 @app.route('/hello')
 def hello():
     return 'Hello, World! Good morning'
