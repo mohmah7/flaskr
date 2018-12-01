8500c81974b7ec68314ce22d840aead43172d538
mohmah7 <mohmah7@users.noreply.github.com>
2018-12-01 09:30:39 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -31,8 +31,8 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World! Good morning,And  welcome'
     
-    from . import db
-    db.init_app(app)
+    #from . import db
+    #db.init_app(app)
     
     return app
 
