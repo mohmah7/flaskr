2eed2b7d324749f88792dbd7e6c3eb012f0bf1bc
mohmah7 <mohmah7@users.noreply.github.com>
2018-12-02 20:26:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World! Good morning,And  welcome'
     
-    from . import db
+    from flaskr import db
     db.init_app(app)
     from . import auth
     app.register_blueprint(auth.bp)
