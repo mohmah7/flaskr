f6b1660d58d9d01d4fa5485a60deafaad997a201
mohmah7 <mohmah7@users.noreply.github.com>
2018-12-04 05:50:56 +0200

    Update subscribers.py

--- subscribers.py
+++ subscribers.py
@@ -14,7 +14,7 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, name, phone, email, address, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM subscribers p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('subscribers/index.html', posts=posts)
