82dac48bfdc8a5e6cf574bd79853d85b85e89433
mohmah7 <mohmah7@users.noreply.github.com>
2018-12-04 17:12:24 +0200

    Update subscribers.py

--- subscribers.py
+++ subscribers.py
@@ -13,7 +13,7 @@ bp = Blueprint('subscribers', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, name, phone, email, address, created, author_id, username'
+        'SELECT p.id, name, email, address, created, author_id, username'
         ' FROM subscribers p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
